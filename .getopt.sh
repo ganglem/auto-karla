eecca0e390e27667b9b3d9777c40daf2fb53f3fb
Emilija <emilija1705@gmail.com>
2025-10-13 15:41:06 +0200

    init

---
+++ .getopt.sh
@@ -0,0 +1,15 @@
+# Supported options are (from build.sh):
+# -d | --distribution <distro>
+# -a | --arch <architecture>
+# -v | --verbose
+# -D | --debug
+# -h | --help
+#      --variant <variant>
+#      --version <version>
+#      --subdir <directory-name>
+#      --get-image-path
+#      --no-clean
+#      --clean
+
+BUILD_OPTS_SHORT="d:a:vDh"
+BUILD_OPTS_LONG="distribution:,arch:,verbose,debug,variant:,version:,subdir:,get-image-path,no-clean,clean,help"
